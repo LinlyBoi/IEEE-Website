47271b68c8f03caa8b3199282aab3e33db0c1fbd
LinlyBoi <linlysmolworthy@gmail.com>
2024-09-14 11:08:37 +0300

    tabs

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-
+import { AuthGuard } from './'
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-	providers: [UsersResolver, UsersService],
+	providers: [
+		UsersResolver,
+		UsersService,
+		{
+			provide: APP_GUARD,
+			useClass: AuthGuard,
+		},
+						 ],
 	exports: [UsersService],
 })
 export class UsersModule {}
