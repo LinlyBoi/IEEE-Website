414f9c4e6d665ed408e8589a0f52380f0847bdcc
LinlyBoi <linlysmolworthy@gmail.com>
2024-09-15 20:19:57 +0300

    those don't belong here

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SetMetadata } from '@nestjs/common';
 
-
 async function bootstrap() {
-	require('dotenv').config();
-	export const IS_PUBLIC_KEY = 'isPublic';
-	export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 	const app = await NestFactory.create(AppModule);
 	await app.listen(3000);
 }
